db4ce332ba8abe87b1ece84eebe30a40e85a483c
wobuzhidao158 <602144792@qq.com>
2026-04-04 10:26:31 +0800

    更新 update.py

--- github/workflows/update.py
+++ github/workflows/update.py
@@ -1,17 +1,17 @@
 import requests
 
-# 把这里改成你要拉取的公开M3U地址
+# http://zhibo.cc.cd/api.php?token=OawLE8oN&type=m3u
 SOURCE_URL = "https://example.com/iptv.m3u"
-# 直播源（比如你现在的列表叫iptv.m3u）
+# 直播源（ http://zhibo.cc.cd/api.php?token=OawLE8oN&type=m3u）
 OUTPUT_FILE = "iptv.m3u"
 
 # 拉取内容
 headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(SOURCE_URL, headers=headers, timeout=15)
+response = requests.get(直播源, headers=headers, timeout=15)
 response.encoding = "utf-8"
 
 # 保存到文件
-with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+with open(http://zhibo.cc.cd/api.php?token=OawLE8oN&type=m3u  , "w", encoding="utf-8") as f:
     f.write(response.text)
 
 print("更新完成，已保存到", OUTPUT_FILE)
