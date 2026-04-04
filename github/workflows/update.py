1482d56bd17a59bda816b886e1023b25f5e89635
wobuzhidao158 <602144792@qq.com>
2026-04-04 10:38:21 +0800

    更新 update.py

--- github/workflows/update.py
+++ github/workflows/update.py
@@ -3,7 +3,7 @@ import requests
 # http://zhibo.cc.cd/api.php?token=OawLE8oN&type=txt
 SOURCE_URL = "http://zhibo.cc.cd/api.php?token=0awLE8oN&type=m3u"
 # 保存到仓库里的文件名，比如你叫iptv.m3u就填这个
-OUTPUT_FILE = "直播源"
+OUTPUT_FILE = "直播源.txt"
 
 # 拉取内容
 headers = {"User-Agent": "Mozilla/5.0"}
